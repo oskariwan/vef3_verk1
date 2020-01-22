9600ac06d85e1770e897799d8ce5e500c12cc946
oskariwan <oskariwan40@gmail.com>
2020-01-22 10:46:55 +0000

    update

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
+import os
 from flask import Flask
 from datetime import datetime
-from flask import render_template
-import re
-
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <p><a href="/sida2" title="Síða 2">Síða 2 </a> | <a href="/sida3" title="Síða 3">Síða 3 </a></p>
+    <img src="http://loremflickr.com/600/400" />
+    """ .format(time=the_time)
 
-   
+@app.route('/sida2')
+def page2():
+    return """
+    <h1>Síða 2</h1>
+    <p><a href="/" title="Forsíða">Forsíða </a> | <a href="/sida3" title="Síða 3">Síða 3 </a></p>
+    <p>It is currently cathour.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """ 
+@app.route('/sida3')
+def page3():
+    return render_template('sida3.html')
+
+
+if __name__ == '__main__':
+    app.run()
+    #app.run(debug=True, use_reloader=True)
